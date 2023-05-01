256dd4bb3de14ea40d0e673989e1f7f383bf2f7e
Nelson Trujillo <nelson.trujillo@cobiscorp.com>
2023-05-01 00:23:19 -0500

    fix: generate jwt keys

--- generate_jwtKeys.sh
+++ generate_jwtKeys.sh
@@ -1,4 +1,6 @@
 mkdir jwt
-openssl genrsa -out jwt/rsaPrivateKey.pem 2048 -ii
+openssl genrsa -out jwt/rsaPrivateKey.pem 2048
 openssl rsa -pubout -in jwt/rsaPrivateKey.pem -out jwt/publicKey.pem
-openssl pkcs8 -topk8 -nocrypt -inform pem -in jwt/rsaPrivateKey.pem -outform pem -out jwt/privatKey.pem
+openssl pkcs8 -topk8 -nocrypt -inform pem -in jwt/rsaPrivateKey.pem -outform pem -out jwt/privateKey.pem
+cp jwt/publicKey.pem amazon-cart/src/main/resources 
+cp jwt/privateKey.pem amazon-jwt/src/main/resources
